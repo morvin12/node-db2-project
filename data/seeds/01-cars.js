3bdc45fe0f8c15c62572e1f6df6c5339e1609552
Michael Orvin <morvin1212@gmail.com>
2022-01-18 20:03:42 -0700

    wip

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,39 @@
 // STRETCH
+const cars = [ 
+    {
+        vin: '12345678910121',
+        make: 'porsche',
+        model: 'GT3RS',
+        mileage: '1200',
+        title: 'clean',
+        transmission: 'manual',
+    },
+    {
+        vin: '12345678910122',
+        make: 'porsche',
+        model: 'GT2RS',
+        mileage: '878',
+        title: 'clean',
+        transmission: 'manual',
+    },
+    {
+        vin: '12345678910123',
+        make: 'porsche',
+        model: 'GT3',
+        mileage: '120',
+        title: 'clean',
+        transmission: 'manual',
+    },
+]
+
+// exports.seed = function(knex) {
+//     return knex('cars')
+//     .truncate().then(() => {
+//         return knex('cars').insert(cars)
+//     })
+// }
+
+exports.seed = async function(knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+    }
