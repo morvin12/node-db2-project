5132fd4acc06191707585ba235a4b612afb8b2af
Michael Orvin <morvin1212@gmail.com>
2022-01-18 21:25:09 -0700

    wip

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,6 +1,9 @@
 // DO YOUR MAGIC
 const express = require('express')
 const Car = require('./cars-model')
+const {
+    checkCarId,
+} = require('./cars-middleware')
 
 const router = express.Router()
 
@@ -12,13 +15,8 @@ router.get('/', async (req, res, next) => {
         next(err)
     }
 })
-router.get('/:id', async (req, res, next) => {
-    try {
-        const car = await Car.getById(req.params.id)
-        res.json(car)
-    } catch (err) {
-        next(err)
-    }
+router.get('/:id', checkCarId, async (req, res, next) => {
+    res.json(req.car)
 })
 router.post('/', async (req, res, next) => {
     res.json('posting new car')
